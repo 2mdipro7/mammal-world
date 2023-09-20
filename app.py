3b084fef286893552c4096ad90dab84ed742c95d
2mdipro7 <mm4mashrafi@gmail.com>
2023-09-20 17:41:30 +0600

    Updated

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template, request
-from gradio_client import Client
+from flask import Flask, render_template, request, jsonify
 import json
+import os
+import tempfile
+from gradio_client import Client
 
 app = Flask(__name__)
 
@@ -8,41 +10,45 @@ app = Flask(__name__)
 gradio_api_url = "https://dipro7-mammals-of-india.hf.space/"
 gradio_client = Client(gradio_api_url)
 
-@app.route("/", methods=['GET', 'POST'])
+# Define a temporary directory to store prediction JSON files
+temp_directory = tempfile.mkdtemp()
+
+@app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
-        # Assuming you receive two JSON file paths as a list
-        result_paths = json.loads(request.form['result'])
-        
-        # Ensure that two paths are provided
-        if len(result_paths) != 2:
-            return render_template("error.html", error_message="Expected two result paths.")
+        # Handle URL input and make predictions using Gradio client
+        image_url = request.form.get("image_url")
 
-        # Extract species and behavior result paths
-        species_result_path, behavior_result_path = result_paths
+        if not image_url:
+            return "No image URL provided"
 
         try:
-            # Load the JSON content from the species result path
-            with open(species_result_path, 'r') as species_json_file:
-                species_json_data = json.load(species_json_file)
-                print("Species JSON Data:", species_json_data)
-                pred_label = species_json_data.get("label", "Species not predicted")
-                confidences = species_json_data.get("confidences", [])
-                confidence = max(confidences, key=lambda item: item.get("confidence", 0))["confidence"] * 100
+            # Get predictions from the Gradio model using the provided URL
+            result = gradio_client.predict({
+                "image": image_url
+            }, api_name="/predict")
 
-            # Load the JSON content from the behavior result path
-            with open(behavior_result_path, 'r') as behavior_json_file:
-                behavior_json_data = json.load(behavior_json_file)
-                print("Behavior JSON Data:", behavior_json_data)
-                behavior_label = behavior_json_data.get("behavior", "Behavior not predicted")
+            # Process the prediction result
+            pred_label = result.get("label", "Species not predicted")
+            confidences = result.get("confidences", [])
+            confidence = max(confidences, key=lambda item: item.get("confidence", 0))["confidence"] * 100
 
-            return render_template("result.html", pred_label=pred_label, confidence=confidence, behavior_label=behavior_label, result_paths=result_paths)
-        except (FileNotFoundError, json.JSONDecodeError) as e:
-            error_message = f"Error reading JSON file: {e}"
-            print(error_message)
-            return render_template("error.html", error_message=error_message)
-    else:
-        return render_template("index.html")
+            # Save prediction results to separate JSON files for species and behavior
+            species_prediction_json_path = os.path.join(temp_directory, "species_prediction.json")
+            behavior_prediction_json_path = os.path.join(temp_directory, "behavior_prediction.json")
+
+            with open(species_prediction_json_path, 'w') as species_json_file:
+                species_json_file.write(json.dumps(result))
+
+            with open(behavior_prediction_json_path, 'w') as behavior_json_file:
+                behavior_json_file.write(json.dumps(result))
+
+            return jsonify({"message": "Prediction successful!", "pred_label": pred_label, "confidence": confidence})
+
+        except Exception as e:
+            return str(e)
+
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
