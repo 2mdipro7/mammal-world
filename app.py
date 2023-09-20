70663f434dbfdd344e5b034dd7f4e419ff720ac5
2mdipro7 <mm4mashrafi@gmail.com>
2023-09-20 18:53:20 +0600

    Restored

--- app.py
+++ app.py
@@ -1,54 +1,38 @@
+# Import necessary libraries
 from flask import Flask, render_template, request, jsonify
-import json
-import os
-import tempfile
+from werkzeug.utils import secure_filename
 from gradio_client import Client
 
 app = Flask(__name__)
 
-# Initialize the Gradio client with your API URL
+# Initialize the Gradio client
 gradio_api_url = "https://dipro7-mammals-of-india.hf.space/"
 gradio_client = Client(gradio_api_url)
 
-# Define a temporary directory to store prediction JSON files
-temp_directory = tempfile.mkdtemp()
-
-@app.route("/", methods=["GET", "POST"])
+@app.route('/')
 def index():
-    if request.method == "POST":
-        # Handle URL input and make predictions using Gradio client
-        image_url = request.form.get("image_url")
+    return render_template('index.html')
 
-        if not image_url:
-            return "No image URL provided"
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'file' not in request.files:
+        return jsonify({"error": "No file part"})
 
-        try:
-            # Get predictions from the Gradio model using the provided URL
-            result = gradio_client.predict({
-                "image": image_url
-            }, api_name="/predict")
+    file = request.files['file']
+    
+    if file.filename == '':
+        return jsonify({"error": "No selected file"})
 
-            # Process the prediction result
-            pred_label = result.get("label", "Species not predicted")
-            confidences = result.get("confidences", [])
-            confidence = max(confidences, key=lambda item: item.get("confidence", 0))["confidence"] * 100
+    if file:
+        # Save the uploaded image to a temporary location
+        filename = secure_filename(file.filename)
+        image_path = 'uploads/' + filename
+        file.save(image_path)
 
-            # Save prediction results to separate JSON files for species and behavior
-            species_prediction_json_path = os.path.join(temp_directory, "species_prediction.json")
-            behavior_prediction_json_path = os.path.join(temp_directory, "behavior_prediction.json")
+        # Make a prediction using the Gradio client
+        result = gradio_client.predict(image_path, api_name="/predict")
 
-            with open(species_prediction_json_path, 'w') as species_json_file:
-                species_json_file.write(json.dumps(result))
+        return jsonify(result)
 
-            with open(behavior_prediction_json_path, 'w') as behavior_json_file:
-                behavior_json_file.write(json.dumps(result))
-
-            return jsonify({"message": "Prediction successful!", "pred_label": pred_label, "confidence": confidence})
-
-        except Exception as e:
-            return str(e)
-
-    return render_template("index.html")
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
